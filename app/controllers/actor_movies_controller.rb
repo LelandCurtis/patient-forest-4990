cca2dd29d39bcc90b27b1719191a55eaa1de98ab
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-13 21:00:54 -0700

    type fix.

--- app/controllers/actor_movies_controller.rb
+++ app/controllers/actor_movies_controller.rb
@@ -1,6 +1,8 @@
 class ActorMoviesController < ApplicationController
   def create
     actor = Actor.where(name: params[:name])
-    application = Application.find(params[:movie_id])
+    movie = Movie.find(params[:movie_id])
+    ActorMovie.create(actor_id: actor.id, movie_id: movie.id)
+    redirect_to "/movies/#{movie.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/studios', to: 'studios#index'
 
   get '/movies/:id', to: 'movies#show'
-  post '/actor_movies', :to 'actor_movies#create'
+
+  post '/actor_movies', to: 'actor_movies#create'
 
 end
