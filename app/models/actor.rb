d52be95f5f33a7eea6a1a22d359450099cc37a2e
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-13 20:34:53 -0700

    Write average age model method for class.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ApplicationRecord
   has_many :actor_movies
   has_many :movies, through: :actor_movies
+
+  def self.average_age
+    ages = pluck(:age)
+    avg = ages.sum / ages.count.to_f
+  end
 end
