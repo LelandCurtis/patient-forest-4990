34845411e47dd465cbc8f6f4044a677e2f528f8f
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-13 21:04:55 -0700

    select a single actor, not the array.

--- app/controllers/actor_movies_controller.rb
+++ app/controllers/actor_movies_controller.rb
@@ -1,6 +1,6 @@
 class ActorMoviesController < ApplicationController
   def create
-    actor = Actor.where(name: params[:name])
+    actor = Actor.where(name: params[:name]).first
     movie = Movie.find(params[:movie_id])
     ActorMovie.create(actor_id: actor.id, movie_id: movie.id)
     redirect_to "/movies/#{movie.id}"
