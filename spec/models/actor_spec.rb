7a898875713e66259baa66afcf9eee727f5bf1de
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-13 20:32:01 -0700

    add actors to controller and add list of actor names to show page. Write model test for average age.

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 RSpec.describe Actor, type: :model do
   it {should have_many(:actor_movies)}
   it {should have_many(:movies).through(:actor_movies)}
+
+  describe 'average_age' do
+    it "returns the average age of all actors" do
+      actor_1 = Actor.create!(name: 'bob', age: 10)
+      actor_2 = Actor.create!(name: 'steve', age: 20)
+      expect(Actor.average_age).to eq(15)
+    end
+  end
 end
