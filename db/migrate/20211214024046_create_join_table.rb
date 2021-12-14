586a68d7b4dd724456191e01d6a3d8d14d644ae4
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-13 20:09:45 -0700

    Redo migrations to work properly.

--- db/migrate/20211214024046_create_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTable < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :actors, :movies do |t|
-      t.index :actor_id
-      t.index :movie_id
-    end
-  end
-end
